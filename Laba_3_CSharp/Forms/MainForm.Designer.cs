e7af6c7b79fe55f565b94132232b7b54e14e93e6
Fluffy Pie <kiernellyt@gmail.com>
2020-04-17 13:26:57 +0300

    SizeFix

--- Laba_3_CSharp/Forms/MainForm.Designer.cs
+++ Laba_3_CSharp/Forms/MainForm.Designer.cs
@@ -33,17 +33,25 @@
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.MainChartWindow = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.RadiusUpDownValue = new System.Windows.Forms.NumericUpDown();
+            this.XRightUpDownValue = new System.Windows.Forms.NumericUpDown();
+            this.XLeftUpDownValue = new System.Windows.Forms.NumericUpDown();
+            this.LeftXLabel = new System.Windows.Forms.Label();
+            this.RightXLabel = new System.Windows.Forms.Label();
+            this.RadiusLabel = new System.Windows.Forms.Label();
+            this.StepLabel = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.TableButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.MainChartWindow)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RadiusUpDownValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XRightUpDownValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XLeftUpDownValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // bunifuElipse1
-            // 
-            this.bunifuElipse1.ElipseRadius = 25;
-            this.bunifuElipse1.TargetControl = this;
             // 
             // MainChartWindow
             // 
@@ -51,7 +59,7 @@
             this.MainChartWindow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             chartArea1.Name = "ChartArea";
             this.MainChartWindow.ChartAreas.Add(chartArea1);
-            this.MainChartWindow.Location = new System.Drawing.Point(23, 35);
+            this.MainChartWindow.Location = new System.Drawing.Point(-7, 39);
             this.MainChartWindow.Name = "MainChartWindow";
             series1.ChartArea = "ChartArea";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
@@ -67,33 +75,178 @@
             series3.MarkerColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
             series3.Name = "Series3";
+            series4.ChartArea = "ChartArea";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Name = "Series4";
+            series5.ChartArea = "ChartArea";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.MarkerBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            series5.MarkerColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            series5.MarkerSize = 7;
+            series5.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series5.Name = "Series5";
             this.MainChartWindow.Series.Add(series1);
             this.MainChartWindow.Series.Add(series2);
             this.MainChartWindow.Series.Add(series3);
-            this.MainChartWindow.Size = new System.Drawing.Size(698, 474);
+            this.MainChartWindow.Series.Add(series4);
+            this.MainChartWindow.Series.Add(series5);
+            this.MainChartWindow.Size = new System.Drawing.Size(630, 470);
             this.MainChartWindow.TabIndex = 0;
             this.MainChartWindow.Text = "Chart";
             // 
-            // trackBar1
+            // RadiusUpDownValue
             // 
-            this.trackBar1.Location = new System.Drawing.Point(800, 73);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(166, 45);
-            this.trackBar1.TabIndex = 2;
-            this.trackBar1.ValueChanged += new System.EventHandler(this.ValueChanged);
+            this.RadiusUpDownValue.DecimalPlaces = 2;
+            this.RadiusUpDownValue.Location = new System.Drawing.Point(629, 196);
+            this.RadiusUpDownValue.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.RadiusUpDownValue.Name = "RadiusUpDownValue";
+            this.RadiusUpDownValue.Size = new System.Drawing.Size(97, 20);
+            this.RadiusUpDownValue.TabIndex = 4;
+            this.RadiusUpDownValue.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.RadiusUpDownValue.ValueChanged += new System.EventHandler(this.RadiusValueChanged);
+            // 
+            // XRightUpDownValue
+            // 
+            this.XRightUpDownValue.DecimalPlaces = 2;
+            this.XRightUpDownValue.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.XRightUpDownValue.Location = new System.Drawing.Point(629, 143);
+            this.XRightUpDownValue.Name = "XRightUpDownValue";
+            this.XRightUpDownValue.Size = new System.Drawing.Size(97, 20);
+            this.XRightUpDownValue.TabIndex = 5;
+            this.XRightUpDownValue.ValueChanged += new System.EventHandler(this.RightXValueChanged);
+            // 
+            // XLeftUpDownValue
+            // 
+            this.XLeftUpDownValue.DecimalPlaces = 2;
+            this.XLeftUpDownValue.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.XLeftUpDownValue.Location = new System.Drawing.Point(629, 94);
+            this.XLeftUpDownValue.Maximum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.XLeftUpDownValue.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.XLeftUpDownValue.Name = "XLeftUpDownValue";
+            this.XLeftUpDownValue.Size = new System.Drawing.Size(97, 20);
+            this.XLeftUpDownValue.TabIndex = 6;
+            this.XLeftUpDownValue.ValueChanged += new System.EventHandler(this.LeftXValueChanged);
+            // 
+            // LeftXLabel
+            // 
+            this.LeftXLabel.AutoSize = true;
+            this.LeftXLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LeftXLabel.Location = new System.Drawing.Point(607, 75);
+            this.LeftXLabel.Name = "LeftXLabel";
+            this.LeftXLabel.Size = new System.Drawing.Size(136, 16);
+            this.LeftXLabel.TabIndex = 7;
+            this.LeftXLabel.Text = "Left border of X";
+            // 
+            // RightXLabel
+            // 
+            this.RightXLabel.AutoSize = true;
+            this.RightXLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RightXLabel.Location = new System.Drawing.Point(607, 124);
+            this.RightXLabel.Name = "RightXLabel";
+            this.RightXLabel.Size = new System.Drawing.Size(144, 16);
+            this.RightXLabel.TabIndex = 8;
+            this.RightXLabel.Text = "Right border of X";
+            // 
+            // RadiusLabel
+            // 
+            this.RadiusLabel.AutoSize = true;
+            this.RadiusLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RadiusLabel.Location = new System.Drawing.Point(608, 177);
+            this.RadiusLabel.Name = "RadiusLabel";
+            this.RadiusLabel.Size = new System.Drawing.Size(128, 16);
+            this.RadiusLabel.TabIndex = 10;
+            this.RadiusLabel.Text = "Value of Radius";
+            // 
+            // StepLabel
+            // 
+            this.StepLabel.AutoSize = true;
+            this.StepLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StepLabel.Location = new System.Drawing.Point(629, 229);
+            this.StepLabel.Name = "StepLabel";
+            this.StepLabel.Size = new System.Drawing.Size(80, 16);
+            this.StepLabel.TabIndex = 12;
+            this.StepLabel.Text = "Step of X";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Location = new System.Drawing.Point(629, 248);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(97, 20);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // gunaElipse1
+            // 
+            this.gunaElipse1.Radius = 25;
+            this.gunaElipse1.TargetControl = this;
+            // 
+            // TableButton
+            // 
+            this.TableButton.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TableButton.Location = new System.Drawing.Point(610, 419);
+            this.TableButton.Name = "TableButton";
+            this.TableButton.Size = new System.Drawing.Size(141, 45);
+            this.TableButton.TabIndex = 13;
+            this.TableButton.Text = "button1";
+            this.TableButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1094, 558);
-            this.Controls.Add(this.trackBar1);
+            this.ClientSize = new System.Drawing.Size(764, 508);
+            this.Controls.Add(this.TableButton);
+            this.Controls.Add(this.StepLabel);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.RadiusLabel);
+            this.Controls.Add(this.RightXLabel);
+            this.Controls.Add(this.LeftXLabel);
+            this.Controls.Add(this.XLeftUpDownValue);
+            this.Controls.Add(this.XRightUpDownValue);
+            this.Controls.Add(this.RadiusUpDownValue);
             this.Controls.Add(this.MainChartWindow);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
             this.Text = "MainForm";
             ((System.ComponentModel.ISupportInitialize)(this.MainChartWindow)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RadiusUpDownValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XRightUpDownValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XLeftUpDownValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,8 +254,17 @@
 
         #endregion
 
-        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
         private System.Windows.Forms.DataVisualization.Charting.Chart MainChartWindow;
-        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.NumericUpDown XRightUpDownValue;
+        private System.Windows.Forms.NumericUpDown RadiusUpDownValue;
+        private System.Windows.Forms.NumericUpDown XLeftUpDownValue;
+
+        private System.Windows.Forms.Label StepLabel;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label RadiusLabel;
+        private System.Windows.Forms.Label RightXLabel;
+        private System.Windows.Forms.Label LeftXLabel;
+        private Guna.UI.WinForms.GunaElipse gunaElipse1;
+        private System.Windows.Forms.Button TableButton;
     }
 }
--- Laba_3_CSharp/Forms/MainForm.cs
+++ Laba_3_CSharp/Forms/MainForm.cs
@@ -15,35 +15,64 @@ namespace Laba_3_CSharp.Forms
         public MainForm()
         {
             InitializeComponent();
-     
-            MainChartWindow.ChartAreas[0].CursorX.IsUserEnabled = true;
-            MainChartWindow.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
-            MainChartWindow.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
-            MainChartWindow.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
-            trackBar1.Maximum = 10000;
-            
            
+            double Radius = Convert.ToDouble(RadiusUpDownValue.Value);
+            MainChartWindow.ChartAreas[0].AxisX.ScaleView.Size = Radius * 5.5;
+            MainChartWindow.ChartAreas[0].AxisY.ScaleView.Size = Radius * 4;
+            DrawAGraph();
+            
         }
-        Code.Calculations Calculations = new Code.Calculations();
-        
-        private void ValueChanged(object sender, EventArgs e)
-        {
 
+        Code.Calculations Calculations = new Code.Calculations();
+        private void DrawAGraph()
+        {
             MainChartWindow.Series[0].Points.Clear();
             MainChartWindow.Series[1].Points.Clear();
             MainChartWindow.Series[2].Points.Clear();
-            double Radius = 1;
-            double x = Convert.ToDouble(trackBar1.Value)/1000;
-            for (double t = 0; t < x; t += 0.1)
+            MainChartWindow.Series[3].Points.Clear();
+            MainChartWindow.Series[4].Points.Clear();
+            double Radius = Convert.ToDouble(RadiusUpDownValue.Value);
+            double XRight = Convert.ToDouble(XRightUpDownValue.Value);
+            double XLeft = Convert.ToDouble(XLeftUpDownValue.Value);
+            MainChartWindow.ChartAreas[0].AxisX.ScaleView.Size = Radius*5.5;
+            MainChartWindow.ChartAreas[0].AxisY.ScaleView.Size = Radius*4;
             {
-                MainChartWindow.Series[0].Points.AddXY(Calculations.AxisXCalculation(t, Radius), Calculations.AxisYCalculation(t, Radius));
+                for (double t = XLeft; t < XRight; t += 0.1)
+                {
+                    MainChartWindow.Series[0].Points.AddXY(Calculations.AxisXCalculation(t, Radius), Calculations.AxisYCalculation(t, Radius));
+                }
+                for (double t = 0; t < 2 * Math.PI; t += 0.1)
+                {
+                    MainChartWindow.Series[1].Points.AddXY(Radius * (XLeft + Math.Cos(t)), Radius * (Math.Sin(t) + 1));
+                }
+                MainChartWindow.Series[2].Points.AddXY(Radius * (XLeft - Math.Sin(XLeft)), Radius * (1 - Math.Cos(XLeft)));
+                for (double t = 0; t < 2 * Math.PI; t += 0.1)
+                {
+                    MainChartWindow.Series[3].Points.AddXY(Radius * (XRight + Math.Cos(t)), Radius * (Math.Sin(t) + 1));
+                }
+                MainChartWindow.Series[4].Points.AddXY(Radius * (XRight - Math.Sin(XRight)), Radius * (1 - Math.Cos(XRight)));
             }
-            for (double t = 0; t < 2 * Math.PI; t += 0.1)
-            {
-                MainChartWindow.Series[1].Points.AddXY(x + Math.Cos(t), Math.Sin(t) + 1);
-            }
-            MainChartWindow.Series[2].Points.AddXY(x - Math.Sin(x), 1 - Math.Cos(x));
-           
         }
-    }
+
+        private void RadiusValueChanged(object sender, EventArgs e)
+        {
+            DrawAGraph();
+        }
+
+        private void LeftXValueChanged(object sender, EventArgs e)
+        {
+            double Radius = Convert.ToDouble(RadiusUpDownValue.Value);
+            double XLeft = Convert.ToDouble(XLeftUpDownValue.Value);
+            MainChartWindow.ChartAreas[0].AxisX.ScaleView.Scroll(XLeft-Radius*10);
+            DrawAGraph();
+        }
+
+        private void RightXValueChanged(object sender, EventArgs e)
+        {
+            double Radius = Convert.ToDouble(RadiusUpDownValue.Value);
+            double XRight= Convert.ToDouble(XRightUpDownValue.Value);
+            MainChartWindow.ChartAreas[0].AxisX.ScaleView.Scroll(XRight+Radius*10);
+            DrawAGraph();
+        }
+    }  
 }
